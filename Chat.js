e760bd559a9cf60a3eb18bdb4b5deca028ee586c
ElijahVaron <145287749+ElijahVaron@users.noreply.github.com>
2023-10-08 10:28:09 -0400

    Add files via upload

---
+++ Chat.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class Chat extends Component {
+
+    componentDidMount() {
+
+    (function(d, m){
+        /*insert link from the Kommunicate app
+/* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+
+    }
+    render(){
+        return(
+            <div>
+            
+            </div>
+        )
+    }
+}
+
+export default Chat
